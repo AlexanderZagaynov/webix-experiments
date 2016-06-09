c835b4ad6a5822751f002f3cc9d4d69564173f28
Alexander Zagaynov <zalex80@gmail.com>
2016-06-09 18:59:44 +0530

    allow CORS and cleanup Gemfile

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
 require 'rails'
@@ -11,5 +13,11 @@ Bundler.require *Rails.groups
 module WebixExperiments
   class Application < Rails::Application
     config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: :any
+      end
+    end
   end
 end
