1437ca9872994cb316f34ee630099697c17fe669
Alexander Zagaynov <zalex80@gmail.com>
2016-06-09 18:22:19 +0530

    render webix-compatible currencies json

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,6 +1,27 @@
 class CurrenciesController < ApplicationController
+  DEFAULT_LIMIT = 25
+  LARGEST_LIMIT = 100
+
   def index
-    @currencies = Currency.all
-    render json: @currencies
+    @start = params[:start].to_i.abs
+    @limit = params[:count].to_i.abs
+    @limit = @limit.zero? ? DEFAULT_LIMIT : [@limit, LARGEST_LIMIT].min
+    @first_time = !params[:continue]
+
+    @currencies = Currency.unscoped.order(:id).offset(@start).limit(@limit)
+
+    output =
+    {
+      data: @currencies.map do |currency|
+        {
+          id:   currency.id,
+          name: currency.name,
+        }
+      end,
+      pos: @start,
+    }
+    output.merge! total_count: @currencies.unscoped.count if @first_time
+
+    render json: output
   end
 end
